cc1d61dafae53388accdfe2df4d1bfa3064be0b1
Al Amin Khan <alaminkhan2010@gmail.com>
2025-12-11 19:26:35 +0600

    feat: add Loading spinner for better UX

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = ({message}) => {
+    return (
+        <div className="text-center py-3">
+            <div className="inline-block animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+            <p className="mt-2 text-gray-600">{message}...</p>
+        </div>
+    );
+};
+
+export default Loading;
